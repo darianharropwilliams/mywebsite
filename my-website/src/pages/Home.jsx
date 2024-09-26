5c533dc4dbfd02b23b5845a6751153e51d967cb7
DarianHarropWilliams <darianharropwilliams@gmail.com>
2024-09-26 12:59:35 -0400

    Created basic website template

---
+++ my-website/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// src/pages/Home.js
+function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Website</h1>
+        <p>This is the home page where you'll find my bio.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
