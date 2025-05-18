7b3dd6d1c5b61da3c3d83387ee46c92dbf790821
DarianHarropWilliams <darianharropwilliams@gmail.com>
2025-05-18 02:02:54 -0400

    added healthz

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,6 @@ const contactLimiter = rateLimit({
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("MongoDB connected");
 }).catch((err) => {
   console.error("MongoDB connection error:", err.message);
 });
@@ -47,5 +46,8 @@ app.use('/api', skillRoutes);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+});
+
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
 });
